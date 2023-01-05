31fcc77c56362df931d7daa7baefb6f128df166e
Andre Aguayo <andre.aguayo@hotmail.com>
2023-01-05 20:08:23 -0300

    Adress model

---
+++ app/Models/Address.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Address extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_uuid',
+        'UF',
+        'city',
+        'district',
+        'street',
+        'number',
+        'reference'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime'
+    ];
+
+    /**
+     * Get the user for the address.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
